2405b5bb4ac4f7d1c2a14551d4a6f9527f3b9d66
Kaike <kaike.miranda@etipi.pi.gov.br>
2025-12-15 11:21:14 -0300

    feat: add repositories

---
+++ src/main/java/com/dias/nutri_plus/repositories/FoodRepository.java
@@ -0,0 +1,11 @@
+package com.dias.nutri_plus.repositories;
+
+import com.dias.nutri_plus.entities.Food;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface FoodRepository extends JpaRepository<Food, UUID> {
+}
---
+++ src/main/java/com/dias/nutri_plus/repositories/MealRepository.java
@@ -0,0 +1,11 @@
+package com.dias.nutri_plus.repositories;
+
+import com.dias.nutri_plus.entities.Meal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface MealRepository extends JpaRepository<Meal, UUID> {
+}
