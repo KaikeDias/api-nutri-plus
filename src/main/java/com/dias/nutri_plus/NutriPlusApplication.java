dcebfe1c1bb1d866cf65d11dc80d044679dc1fd5
Kaike <kaike.miranda@etipi.pi.gov.br>
2025-12-15 08:52:32 -0300

    initial commit

---
+++ src/main/java/com/dias/nutri_plus/NutriPlusApplication.java
@@ -0,0 +1,13 @@
+package com.dias.nutri_plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriPlusApplication.class, args);
+	}
+
+}
