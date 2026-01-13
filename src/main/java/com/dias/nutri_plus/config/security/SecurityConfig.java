2233a53b270adf8033921c447205687d0cbd96ae
Kaike <kaikedias98@gmail.com>
2026-01-13 16:04:13 -0300

    chore: add authentication config

--- src/main/java/com/dias/nutri_plus/config/security/SecurityConfig.java
+++ src/main/java/com/dias/nutri_plus/config/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.dias.nutri_plus.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -9,10 +10,15 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
   @Bean
-  public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-    httpSecurity
-        .csrf(AbstractHttpConfigurer::disable);
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                    .anyRequest().authenticated())
+            .oauth2ResourceServer(oauth2 -> oauth2
+                    .jwt(Customizer.withDefaults())
+            );
 
-    return httpSecurity.build();
+    return http.build();
   }
 }
