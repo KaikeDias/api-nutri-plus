26bfdea59100347bd61257cf2915495cb410c943
Kaike <kaike.miranda@etipi.pi.gov.br>
2026-01-15 11:23:25 -0300

    feat: add test database

--- src/test/java/com/dias/nutri_plus/NutriPlusApplicationTests.java
+++ src/test/java/com/dias/nutri_plus/NutriPlusApplicationTests.java
@@ -2,8 +2,10 @@ package com.dias.nutri_plus;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class NutriPlusApplicationTests {
 
 	@Test
