de317e7311f7657dd15c6d1512652e7feb94aa8f
Kaike <kaike.miranda@etipi.pi.gov.br>
2026-01-15 13:09:49 -0300

    chore: add session management stateless

--- src/main/java/com/dias/nutri_plus/config/security/SecurityConfig.java
+++ src/main/java/com/dias/nutri_plus/config/security/SecurityConfig.java
@@ -4,15 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
   @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+  public SecurityFilterChain filterChain(HttpSecurity http) {
     http
             .csrf(csrf -> csrf.disable())
+            .sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
             .authorizeHttpRequests(auth -> auth
                     .anyRequest().authenticated())
             .oauth2ResourceServer(oauth2 -> oauth2
